1d0edd6cf2c3555279bae304e2891ef194c4133c
HumbledByte <mariodmorales127@gmail.com>
2022-09-27 16:00:39 -0500

    Saving before executing

---
+++ email_sender.py
@@ -0,0 +1,28 @@
+from email.message import EmailMessage
+from app2 import password
+import os
+import ssl
+import smtplib
+
+email_sender = os.environ.get('EMAIL')
+email_password = os.environ.get('GMAIL_KEY')
+
+email_receiver = 'radaka2462@dnitem.com'
+
+subject = "Testing out email"
+body = """
+This is the body of the email
+"""
+
+em = EmailMessage()
+
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_context
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
